6ce17f0c0462acc945fa527a57d3c617756af363
Jay SinnaduraI <jay.sinnadurai@gmail.com>
2024-08-29 19:18:55 +0000

    Add docstring to get username function

--- run.py
+++ run.py
@@ -9,3 +9,16 @@ def get_username():
     return username
 
 
+def instructions():
+    print(f"\nHello {get_username()}, welcome to Battleships!")
+
+    print("\nYour objective is to find and destroy all the hidden ships on the map!\n")
+
+    print(f"""\nHow To Play:
+    \nYou have {INITIAL_AMMO} ammunitions and there are {INITIAL_SHIPS} hidden ships on the map.""")
+
+    print("In order to hit them, you must guess the coordinates of their locations. For example:")
+
+    print("For the first row and first column, you would guess row 1 and column 1.")
+
+    print("Good luck!\n")
